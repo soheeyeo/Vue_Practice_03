93b032fdf052f4f6763c9994321bda8068799af3
soheeyeo <sohee407@gmail.com>
2023-06-29 21:49:52 +0900

    tab UI 02

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <img src="./assets/logo.png" class="logo" />
     </div>
 
-    <Container :게시물데이터="게시물데이터" />
+    <Container :게시물데이터="게시물데이터" :step="step" />
 
     <button @click="more">더보기</button>
 
@@ -21,13 +21,6 @@
       </ul>
     </div>
   </div>
-
-  <div v-if="step === 0">내용0</div>
-  <div v-if="step === 1">내용1</div>
-  <div v-if="step === 2">내용2</div>
-  <button @click="step = 0">버튼0</button>
-  <button @click="step = 1">버튼1</button>
-  <button @click="step = 2">버튼2</button>
 
 </template>
 
@@ -43,7 +36,7 @@ export default {
   name: 'App',
   data(){
     return {
-      step : 0,
+      step : 1,
       게시물데이터: postingData,
       더보기 : 0,
     }
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
+        <div v-if="step === 0" >
         <Post :게시물="게시물데이터[i]" v-for="(a, i) in 게시물데이터" :key="i" />
+        </div>
+
+        <!-- 필터선택페이지 -->
+        <div v-if="step === 1">
+            <div class="upload-image"></div>
+            <div class="filters">
+                <div class="filter-1"></div>
+                <div class="filter-1"></div>
+                <div class="filter-1"></div>
+                <div class="filter-1"></div>
+                <div class="filter-1"></div>
+            </div>
+        </div>
+
+        <!-- 글작성페이지 -->
+        <div v-if="step === 2">
+            <div class="upload-image"></div>
+            <div class="write">
+                <textarea class="write-box">write!</textarea>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -13,6 +35,7 @@ export default {
         Post,
     },
     props : {
+        step : Number,
         게시물데이터 : Array,
     },
 }
@@ -20,4 +43,46 @@ export default {
 
 <style>
 
+.upload-image{
+width: 100%;
+height: 450px;
+background: cornflowerblue;
+background-size : cover;
+}
+.filters{
+overflow-x:scroll;
+white-space: nowrap;
+}
+.filter-1 {
+width: 100px;
+height: 100px;
+background-color: cornflowerblue;
+margin: 10px 10px 10px auto;
+padding: 8px;
+display: inline-block;
+color : white;
+background-size: cover;
+}
+.filters::-webkit-scrollbar {
+height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+background: #f1f1f1; 
+}
+.filters::-webkit-scrollbar-thumb {
+background: #888; 
+border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+background: #555; 
+}
+.write-box {
+border: none;
+width: 90%;
+height: 100px;
+padding: 15px;
+margin: auto;
+display: block;
+outline: none;
+}
 </style>
