75b60750e0b05797c7461bccd81d6a6e345cb0da
soheeyeo <sohee407@gmail.com>
2023-06-29 19:16:28 +0900

    style data binding

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Post :게시물="게시물데이터[i]" v-for="(a, i) in 게시물데이터" :key="i" />
+    </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name : 'ContainerBox',
+    components : {
+        Post,
+    },
+    props : {
+        게시물데이터 : Array,
+    },
+}
+</script>
+
+<style>
+
+</style>
