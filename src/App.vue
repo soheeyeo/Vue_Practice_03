833955ff532b990e9be04d52b8f1f8950b618d31
soheeyeo <sohee407@gmail.com>
2023-06-29 21:36:30 +0900

    tab UI

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,14 @@
       </ul>
     </div>
   </div>
+
+  <div v-if="step === 0">내용0</div>
+  <div v-if="step === 1">내용1</div>
+  <div v-if="step === 2">내용2</div>
+  <button @click="step = 0">버튼0</button>
+  <button @click="step = 1">버튼1</button>
+  <button @click="step = 2">버튼2</button>
+
 </template>
 
 <script>
@@ -35,6 +43,7 @@ export default {
   name: 'App',
   data(){
     return {
+      step : 0,
       게시물데이터: postingData,
       더보기 : 0,
     }
