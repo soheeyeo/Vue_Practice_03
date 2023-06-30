45218343148b3f3f4041202f10bf91e089ee2699
soheeyeo <sohee407@gmail.com>
2023-06-30 14:34:58 +0900

    slot 01

--- src/components/FilterBox.vue
+++ src/components/FilterBox.vue
@@ -1,5 +1,7 @@
 <template>
-    <div :class="필터 + ' filter-item'" :style="`background-image:url(${이미지})`"></div> 
+    <div :class="필터 + ' filter-item'" :style="`background-image:url(${이미지})`">
+        <slot></slot>
+    </div> 
 </template>
 
 <script>
