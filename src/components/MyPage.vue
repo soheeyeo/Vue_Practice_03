25627fea46c8f9f266e492b6dbc60246c55c40fe
soheeyeo <sohee407@gmail.com>
2023-07-02 12:06:13 +0900

    Add MyPage component

---
+++ src/components/MyPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div style="padding : 10px">
+        <h4>팔로워</h4>
+        <input placeholder="?" />
+        <div class="post-header">
+            <div class="profile" style="backgroud-image:url()"></div>
+            <span class="profile-name">{{follower}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+
+export default {
+    name : 'myPage',
+    setup(){
+        let follower = ref([]);
+
+        onMounted(()=>{
+            axios.get('/follower.json').then((a)=>{
+                follower.value = a.data;
+            })
+        })
+
+        return {follower};
+    },
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
